2739c2ba76422a7ce8d1949949657b4e31bcb6aa
Walker Chesley <chesley.walker@gmail.com>
2018-10-10 19:02:49 -0500

    Migrated ePMP logic to Radio class, removed from Main()

--- LMGD_Tester/Radio.cs
+++ LMGD_Tester/Radio.cs
@@ -136,17 +136,26 @@ namespace LMGD_Tester
 
 	 // ePMP Radio Scraping Logic: 
 	    
-        public static string Scrape_ePMP(ChromeDriver browser)
+        public static string Scrape_ePMP(ChromeDriver browser, string RadioIP)
         {
-            //Testing on: 172.28.70.184
-            //string scrapedData = "Nothing found";
-            var userId = browser.FindElementById("CanopyUsername");
-            var password = browser.FindElementById("CanopyPassword");
-            var login = browser.FindElementById("loginbutton");
-            userId.SendKeys("admin");
-            password.SendKeys("amatech1");
-            login.Submit();
+            browser.Navigate().GoToUrl(RadioIP);
+            var userId = browser.FindElementByName("username");
+            var pwd = browser.FindElementByName("password");
+            var login = browser.FindElementById("loginBtn");
             
+            userId.SendKeys("admin");
+            pwd.SendKeys("amatech1");
+            login.Click();
+            /******************************************************************************************
+             * Note for Error handling: 
+             * Max # of users: <span class="error-text">Maximum number of users reached.</span>
+             * Wrong Username/Password: <span class="error-text">Wrong username or password</span>
+            ******************************************************************************************/
+            
+
+
+            //Have to set explicit waits for ePMP DOM to load...wireless connections be slow
+            Thread.Sleep(500);
             var ePMPRssi = browser.FindElementById("dl_rssi").GetAttribute("title");
             var ePMPSNR = browser.FindElementById("dl_snr").GetAttribute("title");
             //var ePMP_EthernetStatus = browser.FindElementsById("alert-success").GetAttribute("title");
@@ -156,30 +165,39 @@ namespace LMGD_Tester
 
             // reboot req handling popup
             // ref stackoverflow: https://stackoverflow.com/questions/12744576/selenium-c-sharp-accept-confirm-box
-
+            string pageSrc = browser.PageSource;
+            
+            //find & click reboot button, currently not visible in DOM 
+            browser.FindElementByClassName("navbar-toggle").Click();
+            Thread.Sleep(500);
             browser.FindElementById("reboot_device").Click();
-             
-            string JSAlertError = null; 
-            string RadioStats = null; 
-            try {
-	            var handleAlert = browser.SwitchTo().Alert();
-	            
+            
+
+            string JSAlertError = null;
+            string RadioStats = null;
+            //Handle pop up asking us if we're sure we want to reboot, yes we are. 
+            try
+            {
+                var handleAlert = browser.SwitchTo().Alert();
+
                 handleAlert.Accept();
-                }
+            }
             catch (Exception e)
             {
-	            JSAlertError = e.ToString();
+                JSAlertError = e.ToString();
                 Console.WriteLine(e.StackTrace);
             }
-            
-	             //Title's in ePMP radio are "preformated", will concantenate strings together and display. 
-	            RadioStats = $"{ePMPUptime.ToString()}\n";
-	            RadioStats += $"{ePMPRssi.ToString()}\n";
-	            RadioStats += $"{ePMPSNR.ToString()}\n";
-	            //RadioStats += $"{ePMP_EthernetStatus.ToString()}\n";
-	            RadioStats += $"{ePMP_DlMod.ToString()}\n";
-	            RadioStats += $"{ePMP_ULMod.ToString()}\n";
-	            return RadioStats; 
+
+            //Title's in ePMP radio are "preformated", will concantenate strings together and display. 
+            RadioStats = $"{ePMPUptime.ToString()}\n";
+            RadioStats += $"{ePMPRssi.ToString()}\n";
+            RadioStats += $"{ePMPSNR.ToString()}\n";
+            RadioStats += $"{ePMP_DlMod.ToString()}\n";
+            RadioStats += $"{ePMP_ULMod.ToString()}\n";
+
+            Console.WriteLine(RadioStats);
+            Console.WriteLine("ePMP Complete...");
+            return RadioStats; 
             
 
              //  return scrapedData;
