e15aab5148b478da72048b26aa07f46e82730587
Walker Chesley <chesley.walker@gmail.com>
2018-10-15 20:30:58 -0500

    Did work on ATA logic

--- LMGD_Tester/Program.cs
+++ LMGD_Tester/Program.cs
@@ -6,8 +6,8 @@ using System.Xml.Linq;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Threading; 
- 
+using System.Threading;
+
 
 //Testing app for new features/Ideas/Groundwork for LMGD app
 
@@ -23,14 +23,14 @@ namespace LMGD_Tester
             string AtaProvisioning = "/tools/ataprovisioning/";
             string SuConfig = "/tools/su_config/default.asp";
             string username;
-            string password; 
-
+            string password;
+            FOPS FOPSPage = new FOPS(); 
             // attempt to load sensitive info from local xml file. 
           
             XElement LMGD_Doc = XElement.Load(@"C:\Users\Walker\Documents\LMGD_Data.xml");
             username = LMGD_Doc.Element("username").Value;
             password = LMGD_Doc.Element("password").Value;
-
+            
 
             //Console.WriteLine("pulled from xml: " + username);
             //Console.WriteLine("pulled from xml: " + password);
@@ -38,90 +38,82 @@ namespace LMGD_Tester
 
             //init chrome browser
             var chromeOptions = new ChromeOptions();
-            chromeOptions.AddArguments("headless","whitelisted-ips=''", @"C:\Users\Walker\AppData\Local\Google\Chrome\User Data\Default\"); //@ home = 1 Default, work = 2 \Default
+            chromeOptions.AddArguments("whitelisted-ips=''", @"user-data-directory=C:\Users\Walker\AppData\Local\Google\Chrome\User Data\Default"); //@ home = 1 Default, work = 2 \Default
             var browser = new ChromeDriver(chromeOptions);
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            //manually loggin in for testing sake. 
+            FOPSPage.FOPS_Login(username, password, browser, FOPS+AtaProvisioning);
+            //browser.Navigate().GoToUrl(FOPS);
+            //I'm getting redirected to login page? session issues?
             //assumes we're logged into FOPS... SHould be if control was transferred from FOPS browser sesh directly...THis should be handled in FOPS browser. 
-            browser.Navigate().GoToUrl("https://fops.amatechtel.com/tools/ataprovisioning/");
-            Console.WriteLine(browser.Url);
-            string AccountNumber = "";
-            string SelectedAtaType; 
-            ////Verify we're on ATA config page. 
-
-            ////Console.WriteLine("Awaiting Login...");
-            ////    var pageTitle = browser.FindElement(By.Id("main_nav_tools_link"));
-
-
-            ////Search for ATA: 
-            //var AccountNumberTxtBox = browser.FindElementById("search_foreign_id");
-            //var searchATA = browser.FindElementById("voip_search_submit_button");
-            //AccountNumberTxtBox.SendKeys(AccountNumber);
-            //searchATA.Click();
-
-            //// Need to handle: No ata found, multiple ata's found, and no connection to FOPS. 
-
-            ////TODO: 
-            ///* 
-            // * Grab All ATA's as list, if only one is found then assume it's the right one and use it. 
-            // * Get ATA type from config page. then navigate control to the proper method. 
-            // * Grab exact match on account number, select that ATA 
-            // */
-            //Console.WriteLine("Searching for ATA...");
-            //var ATA = browser.FindElement(By.Id("search_results_div"));
-
-            ////cust specific ata page is found by unique ata_id
+            
+            //Console.WriteLine(browser.Url);
+            string AccountNumber = "308666";
+            string SelectedAtaType;
+            //browser.Navigate().GoToUrl($"https://fops.amatechtel.com/tools/ataprovisioning/modify.asp?ata_id={AccountNumber}");
+            browser.FindElementById("search_foreign_id").SendKeys(AccountNumber);
+            browser.FindElementById("voip_search_submit_button").Click();
 
 
-            ////strange error searching this element. implicit/explicit waits won't work, attempting to resolve by sleeping the thread.
-            ////Thread.Sleep(150);
 
-            //Console.WriteLine("Awaiting ATA page...");
-            //var AtaTable = browser.FindElementsByClassName("table_row");
-            //var AtaSearchedDiv = browser.FindElementById("search_results_div");
-            //Console.WriteLine($"Searching for ATA: {AtaTable}\n Found: {AtaTable.Count}");
+            browser.FindElementByClassName("table_row").Click();
 
-            //foreach (var Ata in AtaTable)
-            //{
-            //    if (ATA.GetAttribute("ata_id") == AccountNumber)
-            //    {
-
-            //    }
-            //}
-
-
+            //Console.ReadKey();
             //call search url directly as no 'clickable' link in FOPS (lame af Jason)
-            browser.Navigate().GoToUrl($"http://fops.amatechtel.com/tools/ataprovisioning/modify.asp?ata_id={AccountNumber}");
 
-
+            browser.SwitchTo().Window(browser.WindowHandles[1]);
             string whereAmI = browser.Url;
+            string ATAError = "ATA Error occured... ";
             Console.WriteLine($"Number of tabs: {browser.WindowHandles.Count}");
             Console.WriteLine($"Browser Location, after 'searching' ata... {whereAmI}");
-            Console.ReadKey();
+            
 
             //await ATA IP address... 
             Console.WriteLine("Awaiting ATA page...");
 
-            var AtaIP = browser.FindElementByClassName("button tiny expand info radius small_pad pointer");
-            AtaIP.Click();
-            var AtaType = browser.FindElementsByTagName("value");
-            foreach(var ata in AtaType)
-            {
-                if(isAttributePresent((ChromeWebElement)ata, "selected") == true)
-                {
-                    SelectedAtaType = ata.Text; 
-                    //Call proper ATA method here...switch statement?
-                }
-            }
-            //Currntly working up to here, need to hangle migrating to ATA's webpage, ID'ing ATA type, getting information and rebooting ATA, also need to click "save Changes" on ata config page. 
+            var AtaIP = browser.FindElementsByClassName("small_pad");
+            //Should save changes...
+            Console.WriteLine($"Saved changes clicked...{AtaIP[1].Text}");
+            AtaIP[1].Click();
+            Console.WriteLine($"Selecting ATA...{AtaIP[0].Text}");
+            AtaIP[0].Click();
+            // not returning a list like I want... kees finding nothing really. might move ATA detection logic and go a different route, searching DOM instead of FOPS page cause FOPS is gayyyy af fam
+            //var AtaType = browser.FindElementsByTagName("value");
+            //foreach(var ata in AtaType)
+            //{
+            //    Console.WriteLine(ata.Text);
+            //    if(isAttributePresent((ChromeWebElement)ata, "selected") == true)
+            //    {
+            //        SelectedAtaType = ata.Text; 
+            //        //Call proper ATA method here...switch statement? will call proper method depending on what ATA type is selected in DOM. 
+            //        switch(SelectedAtaType)
+            //        {
+            //            case "Cambium 200P":
+            //            case "Cambium R201P":
+            //            Console.WriteLine("Found Cambium ATA, Attempting Login/Reboot...");
+            //                //Call Cambium logic
+            //                break; 
+            //            case "Linksys SPA122":
+            //                Console.WriteLine("Found SPA122, Attempting Login/Reboot...");
+            //                //Call Cisco SPA122 logic
+            //                break; 
+            //            case "Linksys SPA2102":
+            //                Console.WriteLine("Found SPA2102, Attempting Login/Reboot but you might be fucked anyway lol it's a POS...");
+            //                //Call SPA 2102 logic
+            //                break;
+            //            default:
+            //            Console.WriteLine(ATAError);
+            //                break;
+            //        }
+            //    }
+            //}
+            
+             
 
             Console.WriteLine($"If ata found/ip addr clicked number of tabs is now: {browser.WindowHandles.Count}");
-            Console.ReadKey();
-            browser.SwitchTo().Window(browser.WindowHandles[3]); //page[1] holds webpage, page [2] is Data; (?idk what this is really), page [3..n] are tabs we open. 
-            Console.WriteLine("Success?");
-            //TODO: log into ATA, get stats based on ATA Type and reboot ATA (Save config too). or what you wrote above dipshit :P ^^^ just 6 lines
-            Console.WriteLine($"{browser.Url}");
-            Console.WriteLine("End...");
-            Console.ReadKey();
+           
+            browser.SwitchTo().Window(browser.WindowHandles[2]); //page[0] holds FOPS search, 1 has ATA config, 2 should be ATA 
+            
 
            
             
