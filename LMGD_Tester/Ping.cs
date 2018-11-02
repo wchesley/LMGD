0739d63dcae4a7b7f4c5380f6ea13f7596828ec0
Walker Chesley <chesley.walker@gmail.com>
2018-11-01 19:27:49 -0500

    Fixed errors in ping class

--- LMGD_Tester/Ping.cs
+++ LMGD_Tester/Ping.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace LMGD_Tester
 {
-    class Ping
+    class Pinger
     {
         public string PingBuilder(string IP, string equipType)
         {
-            string PingReplies;
+            string PingReplies = string.Empty;
             Ping Pinger = new Ping();
             PingOptions opt = new PingOptions();
-            opt.DontFragment = true; 
+            opt.DontFragment = true;
             int successPacket = 0;
             int failedPacket = 0; 
             //create packet as per https://docs.microsoft.com/en-us/dotnet/api/system.net.networkinformation.ping?view=netframework-4.7.2
@@ -23,22 +23,22 @@ namespace LMGD_Tester
             //this would need to be closed with each new run 
             for(int i=0; i<=10; i++)
             {
-                PingReply reply = Pinger.
-                if(reply == IPStatus.Success)
+                PingReply reply = Pinger.Send(IP,timeout,buffer,opt);
+                if(reply.Status == IPStatus.Success)
                 {
                     Console.WriteLine($"Ping to {reply.Address.ToString()} received in: {reply.RoundtripTime}");
                     PingReplies += $"Ping to {reply.Address.ToString()} received in: {reply.RoundtripTime}";
                     successPacket++;
                 }
                 //if ping fales then RoundtripTime will be 0
-                else if (reply.RoundtripTime = 0)
+                else if (reply.RoundtripTime == 0)
                 {
                     Console.WriteLine($"Failed to receive reply from {IP}");
                     PingReplies += $"Failed to receive reply from {IP}"; 
                     failedPacket++;
                 }
             } 
-            return PingReplies(); 
+            return PingReplies; 
         }
     }
 }
