3d56bb3339bdfcbe4906b7404ed749a5a24c2ea9
wchesley <chesley.walker@gmail.com>
2018-11-07 00:02:31 -0600

    SPA122 logic fleshed out, needs testing

--- LMGD_Tester/ATA.cs
+++ LMGD_Tester/ATA.cs
@@ -97,6 +97,64 @@ namespace LMGD_Tester
         public string Spa122(ChromeDriver browser)
         {
             string ATAInfo = "";
+            browser.FindElementByName("user").SendKeys(userName);
+            browser.FindElementByName("pwd").SendKeys(passWord);
+            //login button? no unique ID for it so trying this way, hopefully I can count 
+            browser.FindElements(By.TagName("input"))[5].Click();
+
+            //Go to network setup page. 
+            browser.FindElementById("trt_Network_Service.asp").Click();
+            var networkSettings = browser.FindElementById("d_4");
+            networkSettings.FindElement(By.TagName("a")).Click();
+            //Show DHCP reservations. 
+            browser.FindElementById("t3").Click();
+            //id's could be numbered like an array? need to find an spa122 with mulitples to test that, too bad they're becoming rare...or is it that bad? lolol
+            var DHCP_Name = browser.FindElementsById("dhcp_select_name_0");
+            var DHCP_IP = browser.FindElementsById("dhcp_select_ip_0");
+            var DHCP_MAC = browser.FindElementsById("dhcp_select_mac_0");
+            ATAInfo += $"Number of items found in DHCP: {DHCP_IP.Count.ToString()}";
+            for (int dhcp_list = 0; dhcp_list <= DHCP_IP.Count; dhcp_list++)
+            {
+                ATAInfo += $"DHCP client name: {DHCP_Name[dhcp_list].Text} IP: {DHCP_IP[dhcp_list].Text} MAC: {DHCP_MAC[dhcp_list].Text}";
+                Console.WriteLine($"DHCP client name: {DHCP_Name[dhcp_list].Text} IP: {DHCP_IP[dhcp_list].Text} MAC: {DHCP_MAC[dhcp_list].Text}");
+            }
+
+            //Check phones
+            browser.FindElementById("trt_voice.asp").Click();
+            //this page is a mess, hope we really REALLY can count...you really don't wanna see this DOM man. but if you do uncomment this next line, ye be warned
+            //Console.WriteLine(browser.PageSource.ToString());
+
+            //Should be leading DIV containing ALL the info related to voice/uptime. 
+            var infoDiv = browser.FindElementById("Information");
+            var infoTable = infoDiv.FindElements(By.TagName("tr"))[8];
+            infoTable = infoTable.FindElements(By.TagName("td"))[3];
+            ATAInfo += $"ATA up time: {infoTable.FindElement(By.TagName("font")).Text}";
+            //Line 1: 
+            infoTable = infoTable.FindElements(By.TagName("tr"))[16];
+            ATAInfo += $"Line 1 is {infoTable.FindElements(By.TagName("font"))[0].Text} hook and {infoTable.FindElements(By.TagName("font"))[1]} to SIP Server";
+
+            //line 2
+            infoTable = infoTable.FindElements(By.TagName("tr"))[42]; //yeah that many rows in this table
+            ATAInfo += $"Line 2 is {infoTable.FindElements(By.TagName("font"))[0].Text} hook and {infoTable.FindElements(By.TagName("font"))[1].Text} to SIP Server";
+            Console.WriteLine("Going to reboot ATA...");
+            browser.FindElementById("trt_Management.asp").Click();
+            var adminPage = browser.FindElementById("d_20");
+            //should be reboot button. 
+            adminPage.FindElement(By.TagName("a")).Click();
+            browser.FindElementById("t4").Click();
+            //handle JS alert 
+            string JSAlertError = null;
+            try
+            {
+                var handleAlert = browser.SwitchTo().Alert();
+
+                handleAlert.Accept();
+            }
+            catch (Exception e)
+            {
+                JSAlertError = e.ToString();
+                Console.WriteLine(e.StackTrace);
+            }
             return ATAInfo;
         }
 
