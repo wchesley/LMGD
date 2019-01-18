cf407720413fad1556b89e5946398d46d92f1bca
wchesley <chesley.walker@gmail.com>
2019-01-17 21:16:45 -0600

    attempted mitigation on 'failure to create chrome session' bug in headless browser.

--- LMGD_Tester/BrowserExt.cs
+++ LMGD_Tester/BrowserExt.cs
@@ -8,12 +8,38 @@ namespace LMGD_Tester
     {
         public ChromeDriver CreateHeadlessBrowser(string URL)
         {
-            var chromeOptions = new ChromeOptions();
-            chromeOptions.AddArguments("headless", "whitelisted-ips='' ","remote-debugging-port=8000", @"user-data-directory=C:\Users\wchesley\AppData\Local\Google\Chrome\User Data\Default"); //@ home = 1 Default, work = 2 \Default
-            var browser = new ChromeDriver(chromeOptions);
-            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(25);
-            browser.Navigate().GoToUrl(URL);
-            return browser;
+            try
+            {
+                var chromeOptions = new ChromeOptions();
+                chromeOptions.AddArguments("headless", "whitelisted-ips='' ", "remote-debugging-port=8000", @"user-data-directory=C:\Users\wchesley\AppData\Local\Google\Chrome\User Data\Default"); //@ home = 1 Default, work = 2 \Default
+                var browser = new ChromeDriver(chromeOptions);
+                browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(25);
+                browser.Navigate().GoToUrl(URL);
+                return browser;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ERROR! Couldn't start chrome session\nWould you like to try the GUI browser instead?\nY/N:");
+                Console.ResetColor(); 
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "y":
+                    case "Y":
+                    case "yes":
+                    case "Yes":
+                        Console.WriteLine("Attempting to start GUI Chrome, Please note this method is much slower than headless...");
+                        var browser = CreateBrowser(URL);
+                        return browser; 
+                    default:
+                        Console.WriteLine($"ERROR: {e.ToString()}\nExiting app...");
+                        Environment.Exit(0);
+                        break;
+                }
+                throw;
+            }
+            
         }
         public ChromeDriver CreateBrowser(string URL)
         {
